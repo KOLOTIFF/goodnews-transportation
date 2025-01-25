404bb62e51d240471cffeb104934e5466fbc2c0b
KOLOTIFF <kologodramane2@gmail.com>
2025-01-25 02:42:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     alert('Welcome to Good News Transportation!');
+
+    function initAutocomplete() {
+        var input1 = document.getElementById('pickup');
+        var input2 = document.getElementById('dropoff');
+        var input3 = document.getElementById('stops');
+
+        new google.maps.places.Autocomplete(input1);
+        new google.maps.places.Autocomplete(input2);
+        new google.maps.places.Autocomplete(input3);
+    }
+
+    initAutocomplete();
 });
